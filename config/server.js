ce0369c01bc1b5c55052b8a04fe1a399f511b959
Timi <itstimiking@gmail.com>
2021-04-10 10:55:48 +0100

    Moved deplyment to github

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e6bc6ec3430b9b5033981014c370144d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e86430f9-e8ff-47b9-bd1b-4bb2d108e7d2'
+};
